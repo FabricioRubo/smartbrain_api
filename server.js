88711f2a035d945648a7e56255ca70b2c113c185
FabricioRubo <48064337+FabricioRubo@users.noreply.github.com>
2021-02-10 21:08:38 -0300

    Adding a envinronment port

--- server.js
+++ server.js
@@ -33,4 +33,4 @@ app.post('/imageURL', (req, res) => {imagePut.handleApiCall(req, res)})
 
 app.put('/image', (req, res) => {imagePut.handleImagePut(req, res, db)});
 
-app.listen(3000, () => {console.log('app is running on port 3000')});
+app.listen(process.env.PORT || 3000, () => {console.log('app is running on port' + process.env.PORT)});
